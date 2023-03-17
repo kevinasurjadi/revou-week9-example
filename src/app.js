f6996a22f0858286cf6f234537ed5975446509c3
kevinasurjadi <gt_kevin_alexander@hotmail.com>
2023-03-18 00:41:39 +0700

    feat: books fix and add library dom and logic

--- src/app.js
+++ src/app.js
@@ -12,6 +12,89 @@ function toggleMobileMenu() {
     }
 }
 
+function loadLibrary() {
+    let searchForm = document.getElementById('search-form');
+    searchForm.addEventListener('submit', searchLibrary);
+}
+
+function searchLibrary(event) {
+    event.preventDefault();
+    let searchInput = document.getElementById('search');
+    let query = searchInput.value;
+
+    let searchResult = document.getElementById('search-result');
+    searchResult.innerHTML = '';
+
+    let wrapper = document.createElement('div');
+    wrapper.className = 'my-6';
+    
+    if (query && query != '') {
+        searchResult.classList.remove('hidden');
+        fetch('./data.json')
+            .then(response => response.json())
+            .then(json => {
+                let books = json['books'];
+                console.log(books);
+                let filteredBooks = books.filter((book) => book['title'].toLowerCase().includes(query) || book['authors'].join(', ').toLowerCase().includes(query) || book['subjects'].join(', ').toLowerCase().includes(query));
+                if (filteredBooks.length > 0) {
+                    filteredBooks.forEach((book) => {
+                        let a = document.createElement('a');
+                        a.href = '#';
+                        a.className = 'flex flex-col items-center bg-white border border-gray-200 p-4 shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700';
+
+                        let img = document.createElement('img');
+                        img.className = 'object-cover w-full rounded-lg h-96 md:h-auto md:w-24';
+                        img.src = book['image'];
+                        img.alt = book['title'];
+
+                        let div = document.createElement('div');
+                        div.className = 'flex flex-col justify-between p-4 leading-normal text-gray-400';
+
+                        let h5 = document.createElement('h5');
+                        h5.className = 'mb-2 text-2xl font-bold tracking-tight';
+                        h5.innerHTML = book['title'];
+
+                        let p = document.createElement('p');
+                        p.className = 'mb-3 font-normal';
+
+                        let b = document.createElement('b');
+                        b.innerHTML = 'Author(s): ';
+
+                        let authors = document.createTextNode(book['authors'].join(', '));
+
+                        p.appendChild(b);
+                        p.appendChild(authors);
+
+                        let caption = document.createElement('caption');
+                        caption.innerHTML = book['subjects'].join(', ');
+
+                        div.appendChild(h5);
+                        div.appendChild(b);
+                        div.appendChild(caption);
+
+                        a.appendChild(img);
+                        a.appendChild(div);
+
+                        wrapper.appendChild(a);
+                    });
+                    searchResult.appendChild(wrapper);
+                } else {
+                    let center = document.createElement('center');
+
+                    let h1 = document.createElement('h1');
+                    h1.className = 'text-gray-400';
+                    h1.innerHTML = 'No books found, please try with another keyword';
+
+                    center.appendChild(h1);
+
+                    searchResult.appendChild(center);
+                }
+            });
+    } else {
+        searchResult.classList.add('hidden');
+    }
+}
+
 function loadBooks() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
@@ -63,7 +146,10 @@ function loadBooks() {
                 let b = document.createElement('b');
                 b.innerHTML = 'Author(s): ';
 
-                p.innerHTML = b + book['authors'];
+                var author = document.createTextNode(book['authors'].join(', '));
+
+                p.appendChild(b);
+                p.appendChild(author);
 
                 innerDiv.appendChild(aH5);
                 innerDiv.appendChild(p);
