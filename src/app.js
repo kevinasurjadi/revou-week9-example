d197eff41c628e791cc9e3c1a85ba9ce1fbbbc46
kevinasurjadi <gt_kevin_alexander@hotmail.com>
2023-03-17 23:59:24 +0700

    feat: books javascript dom and logic

--- src/app.js
+++ src/app.js
@@ -11,3 +11,93 @@ function toggleMobileMenu() {
         mobileMenu.classList.replace('block', 'hidden');
     }
 }
+
+function loadBooks() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    let page = urlParams.get('page');
+
+    if (!page) {
+        page = 1;
+    } else {
+        page = Number(page);
+    }
+
+    const booksPerPage = 12;
+    let bookGrid = document.getElementById('book-grid');
+    bookGrid.innerHTML = '';
+    fetch('./data.json')
+        .then((response) => response.json())
+        .then((json) => {
+            let books = json['books'].slice((page - 1) * booksPerPage, page * booksPerPage);
+            books.forEach(book => {
+                let outerDiv = document.createElement('div');
+                outerDiv.className = 'w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700';
+                
+                let aImg = document.createElement('a');
+                aImg.href = '#';
+
+                let img = document.createElement('img');
+                img.className = 'h-72 mx-auto';
+                img.src = book['image'];
+                img.alt = book['title'];
+
+                aImg.appendChild(img);
+
+                let innerDiv = document.createElement('div');
+                innerDiv.className = 'p-5';
+
+                let aH5 = document.createElement('a');
+                aH5.href = '#';
+
+                let h5 = document.createElement('h5');
+                h5.className = 'mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white';
+                h5.innerHTML = book['title'];
+
+                aH5.appendChild(h5);
+
+                let p = document.createElement('p');
+                p.className = 'mb-3 font-normal text-gray-700 dark:text-gray-400';
+
+                let b = document.createElement('b');
+                b.innerHTML = 'Author(s): ';
+
+                p.innerHTML = b + book['authors'];
+
+                innerDiv.appendChild(aH5);
+                innerDiv.appendChild(p);
+
+                outerDiv.appendChild(aImg);
+                outerDiv.appendChild(innerDiv);
+
+                bookGrid.appendChild(outerDiv);
+            });
+        });
+}
+
+function previousBooksPage() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    let page = urlParams.get('page');
+
+    if (page && page == '2') {
+        window.location.replace('?page=1');
+    } else {
+        window.location.reload();
+    }
+}
+
+function nextBooksPage() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    let page = urlParams.get('page');
+
+    if (page && page == '1') {
+        window.location.replace('?page=2');
+    } else {
+        window.location.reload();
+    }
+}
